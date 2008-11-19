81c763873a40860cebb4563801edfe7aaa6500b8
Teflon Ted <github@rudiment.net>
2008-11-19 13:55:25 -0500

    form now properly casts incoming data

--- server/hook-me-up/hereiam.py
+++ server/hook-me-up/hereiam.py
@@ -16,10 +16,10 @@ class Pimp(webapp.RequestHandler):
   def post(self):
     hooker = Hooker()
     hooker.name = self.request.get('name')
-#    hooker.age = self.request.get('age')
-#    hooker.gender = self.request.get('gender')
-#    hooker.longitude = self.request.get('longitude')
-#    hooker.latitude = self.request.get('latitude')
+    hooker.age = int(self.request.get('age'))
+    hooker.gender = self.request.get('gender')
+    hooker.longitude = float(self.request.get('longitude'))
+    hooker.latitude = float(self.request.get('latitude'))
     hooker.put()
     self.redirect('/')
 
@@ -32,7 +32,11 @@ class MainPage(webapp.RequestHandler):
       self.response.out.write('<li>%s</li>' % hooker.name)
     self.response.out.write("""
           <form action="/hereiam" method="post">
-            <div><input name="name" /></div>
+          <div><input name="name" value="Ted" /></div>
+          <div><input name="age" value="36" /></div>
+          <div><input name="gender" value="m" /></div>
+          <div><input name="longitude" value="-80.4038" /></div>
+          <div><input name="latitude" value="26.1353"/></div>
             <div><input type="submit" value="Hook Me Up"></div>
           </form>""")
     self.response.out.write('</body></html>')
